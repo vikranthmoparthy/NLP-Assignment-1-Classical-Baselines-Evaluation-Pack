a6537c1963c71385b63a0614b4b2bd9c9e34ad1b
Vikranth Moparthy <vikranth.moparthy@gmail.com>
2026-02-19 09:19:56 +0100

    small change

--- source_code/models.py
+++ source_code/models.py
@@ -8,15 +8,15 @@ The source of the sklearn documentation we looked up is below:
 from sklearn.linear_model import LogisticRegression
 from sklearn.svm import LinearSVC
 
-def train_log_reg(X_train, y_train, seed=7): #We train a logistic regression model using TD-IDF features.
+def train_log_reg(x_train, y_train, seed=7): #We train a logistic regression model using TD-IDF features.
     #We set a seed for reproducibility and max iterations of 1000, which was a typical number we set in previous ML projects.
     model = LogisticRegression(random_state=seed, max_iter=1000) 
-    model.fit(X_train, y_train)
+    model.fit(x_train, y_train)
     return model
 
-def train_svm(X_train, y_train, seed=7): #Here we train a linear SVM.
+def train_svm(x_train, y_train, seed=7): #Here we train a linear SVM.
     #Here, we instantiate the model. 
     #The dual auto parameter allows python to automatically choose which way to optimize according to the data shape.
     model = LinearSVC(random_state=seed, dual='auto') 
-    model.fit(X_train, y_train)
+    model.fit(x_train, y_train)
     return model
