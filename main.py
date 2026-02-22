933f3c90284a826b3f73992f92e958bb330ec2d5
Vikranth Moparthy <vikranth.moparthy@gmail.com>
2026-02-22 12:43:22 +0100

    small change

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 RAND_SEED = 7
 LABEL_NAMES = ["World", "Sports", "Business", "Sci/Tech"]
 
-def evaluate_logregmodel(x_train_vec, y_train, x_dev_vec, y_dev, x_test_vec, y_test):
+def evaluate_logregressionmodel(x_train_vec, y_train, x_dev_vec, y_dev, x_test_vec, y_test):
     #We now train the logistic regression model using the vectorized training data.
     lr_model = train_log_reg(x_train_vec, y_train, seed=RAND_SEED) 
     
@@ -58,7 +58,7 @@ def main():
     x_dev_vec = vectorizer.transform(x_dev_text)
     x_test_vec = vectorizer.transform(x_test_text)
     
-    evaluate_logregmodel(x_train_vec, y_train, x_dev_vec, y_dev, x_test_vec, y_test)
+    evaluate_logregressionmodel(x_train_vec, y_train, x_dev_vec, y_dev, x_test_vec, y_test)
     evaluate_svm_plus_errors(x_train_vec, y_train, x_dev_vec, y_dev, x_test_vec, y_test, x_test_text)
 
 if __name__ == "__main__":
